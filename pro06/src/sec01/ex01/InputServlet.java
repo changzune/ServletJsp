8d6fea9f14e2a46e8b47aa0271ca4e45d6494cff
changzunelee <changzunelee@gmail.com>
2021-12-30 23:59:02 +0900

    #

--- pro06/src/sec01/ex01/InputServlet.java
+++ pro06/src/sec01/ex01/InputServlet.java
@@ -16,12 +16,12 @@ public class InputServlet  extends HttpServlet{
    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
                                             throws ServletException, IOException {
       request.setCharacterEncoding("utf-8");
-      String user_id=request.getParameter("user_id");
-      String user_pw=request.getParameter("user_pw"); 
+      String user_id = request.getParameter("user_id");
+      String user_pw = request.getParameter("user_pw"); 
       System.out.println("아이디:"+user_id);
       System.out.println("비밀번호:"+user_pw);
       String[] subject=request.getParameterValues("subject"); 
-      for(String str:subject){
+      for(String str : subject){
          System.out.println("선택한 과목:"+str);
       }
    }
