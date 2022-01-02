c6533907875ea4abb2a1bca6b59e53ef94044c34
changzunelee <changzunelee@gmail.com>
2022-01-02 21:58:04 +0900

    #

--- pro06/src/sec03/ex01/LoginServlet3.java
+++ pro06/src/sec03/ex01/LoginServlet3.java
@@ -1,7 +1,7 @@
 package sec03.ex01;
 
 import java.io.IOException;
-
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,19 +13,32 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/login3")
 public class LoginServlet3 extends HttpServlet {
-	public void init() throws ServletException {
-		System.out.println("init 메서드 호출");
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		String user_id = request.getParameter("user_id");
 		String user_pw = request.getParameter("user_pw");
-		System.out.println("아이디:" + user_id);
-		System.out.println("비밀번호:" + user_pw);
+		System.out.println("아이디: " + user_id);
+		System.out.println("비밀번호: " + user_pw);
+		
 	}
 
-	public void destroy() {
-		System.out.println("destroy 메서드 호출");
-	}
 }
