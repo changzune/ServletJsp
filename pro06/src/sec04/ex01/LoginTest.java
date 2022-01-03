4943fd58eb536ab497f232d9afd9101bc12fc338
changzunelee <changzunelee@gmail.com>
2022-01-04 05:59:42 +0900

    #

--- pro06/src/sec04/ex01/LoginTest.java
+++ pro06/src/sec04/ex01/LoginTest.java
@@ -3,47 +3,79 @@ package sec04.ex01;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/loginTest")
-public class LoginTest extends HttpServlet{  
-   public void init(){	
-      System.out.println("init 메서드 호출");
-   }
+/**
+ * Servlet implementation class LoginTest
+ */
+@WebServlet("/LoginTest")
+public class LoginTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
-   protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-                                                  throws IOException, ServletException{
-      request.setCharacterEncoding("utf-8");   
-      response.setContentType("text/html;charset=utf-8");  
-      PrintWriter out = response.getWriter();				
-      String id = request.getParameter("user_id");  
-      String pw = request.getParameter("user_pw");  
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter()	;
+		String id = request.getParameter("user_id");
+		String pw = request.getParameter("user_pw");
 		
-      System.out.println("아이디   : "+ id);  
-      System.out.println("패스워드 : "+ pw);
-	
-     if(id!= null &&(id.length()!=0)){
-		out.print("<html>");  
-		out.print("<body>");
-		out.print( id +" 님!! 로그인 하셨습니다." );
-		out.print("</html>");
-		out.print("</body>");
-      }else{
-		out.print("<html>");  
-		out.print("<body>");
-		out.print("아이디를 입력하세요!!!" ) ;
-		out.print("<br>");
-		out.print("<a href='http://localhost:8090/pro06/test01/login.html'>로그인창으로 이동  </a>");
-		out.print("</html>");
-		out.print("</body>");
-      }
-   }
+		System.out.println("아이디:" + id);
+		System.out.println("비밀번호: " + pw);
+		
+		if(id !=null &&(id.length()!=0)) {
+			if(id.equals("admin")) {
+				out.print("<html><body>");
+				out.print("<font size='12'>관리자로 로그인 했습니다.</font>");
+				out.print("<br>");
+				out.print("<input type='button' value='회원정보수정하기' />");
+				out.print("<input type='button' value='회원정보삭제하기' />");
+				out.print("</body></html>");
+			}else{
+				out.print("<html>");
+				out.print("<boby>");
+				out.print(id + "님 로그인하였습니다.");
+				out.print("</body>");
+				out.print("</html>");
+			}
+			out.print("<html>");
+			out.print("<boby>");
+			out.print(id + "님 로그인하였습니다.");
+			out.print("</body>");
+			out.print("</html>");
+		}else {
+			out.print("<html>");
+			out.print("<body>");
+			out.print("아이디를 입력하세요");
+			out.print("<br>");
+			out.print("<a href='http://localhost:8090/pro/test01/login.html'>로그인창으로 이동 </a>");
+			out.print("</body>");
+			out.print("</html>");
+		}
+		
+		
+		
+	}
 
-   public void destroy(){
-      System.out.println("destroy 메서드 호출");
-   }
 }
