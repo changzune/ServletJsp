9d5dbee9d66985d39972c8da4850904204258d09
changzunelee <changzunelee@gmail.com>
2022-01-03 23:02:40 +0900

    ##

--- pro06/src/sec03/ex02/LoginServlet4.java
+++ pro06/src/sec03/ex02/LoginServlet4.java
@@ -1,7 +1,7 @@
 package sec03.ex02;
 
 import java.io.IOException;
-
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,30 +13,45 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/login4")
 public class LoginServlet4 extends HttpServlet {
-	public void init() throws ServletException {
-		System.out.println("init 메서드 호출");
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
 	}
 
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("doGet 메서드 호출");
+		System.out.println("doget 메서드호출");
 		doHandle(request, response);
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("doPost 메서드 호출");
+		System.out.println("dopost 메서드호출");
 		doHandle(request, response);
 	}
-
+	
 	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-		System.out.println("doHandle 메서드 호출");
+		System.out.println("doHandle 메세드 호출");
 		String user_id = request.getParameter("user_id");
 		String user_pw = request.getParameter("user_pw");
-		System.out.println("아이디:" + user_id);
-		System.out.println("비밀번호:" + user_pw);
-	}
-
-	public void destroy() {
-		System.out.println("destroy 메서드 호출");
+		System.out.println("아이디: " + user_id);
+		System.out.println("비밀번호 " + user_pw);
+		
 	}
 }
